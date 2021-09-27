955b6d12774f976cb273e913f9b03500fcaaab32
tangx <uyinn@live.com>
2021-09-28 00:11:40 +0800

    chore: 增加了一些 pod 状态信息

--- internal/biz/pod/pod.go
+++ internal/biz/pod/pod.go
@@ -4,15 +4,24 @@ import (
 	"time"
 
 	"github.com/tangx/k8sailor/internal/k8sdao"
+	corev1 "k8s.io/api/core/v1"
 )
 
 type Pod struct {
-	Name      string   `json:"name"`
-	Namespace string   `json:"namespace"`
-	Images    []string `json:"images"`
-	NodeName  string   `json:"nodeName"`
-
+	Name       string    `json:"name"`
+	Namespace  string    `json:"namespace"`
+	Images     []string  `json:"images"`
+	NodeName   string    `json:"nodeName"`
 	CreateTime time.Time `json:"createTime"`
+	PodIP      string    `json:"podIp"`
+	Status     PodStatus `json:"status"`
+	// Status2    corev1.PodStatus
+}
+
+type PodStatus struct {
+	Phase   corev1.PodPhase `json:"phase"`
+	Message string          `json:"message"`
+	Reason  string          `json:"reason"`
 }
 
 type GetPodsByLabelsInput struct {
@@ -36,6 +45,13 @@ func GetPodsByLabels(input GetPodsByLabelsInput) ([]*Pod, error) {
 			Images:     PodImages(v1pod.Spec),
 			NodeName:   v1pod.Spec.NodeName,
 			CreateTime: v1pod.CreationTimestamp.Time,
+			PodIP:      v1pod.Status.PodIP,
+			Status: PodStatus{
+				Phase:   v1pod.Status.Phase,
+				Message: v1pod.Status.Message,
+				Reason:  v1pod.Status.Reason,
+			},
+			// Status2: v1pod.Status,
 		}
 	}
 
