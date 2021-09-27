a374550893827fc1f4d13e9194af7001f2d4dda7
tangx <uyinn@live.com>
2021-09-27 08:41:10 +0800

    feat: bootstrap show deployments

--- webapp/src/components/Deployment.vue
+++ webapp/src/components/Deployment.vue
@@ -5,6 +5,35 @@
 
 {{ data.items }}
 
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Active</th>
+      <th scope="col">Name</th>
+      <th scope="col">Replicas</th>
+      <th scope="col">Images</th>
+      <th scope="col">Status<hr>replicas/availableReplicas/unavailableReplicas</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(item,id) in data.items" key=:id>
+      <th scope="row">{{ id }}</th>
+      <td>active</td>
+      <td>{{ item.name }}</td>
+      <td>{{ item.replicas }}</td>
+      <td>{{ item.images }}</td>
+      <td>
+        <span>{{ item.status.replicas }}</span> <span>/</span> 
+        <span>{{ item.status.availableReplicas }}</span> <span>/</span> 
+        <span>{{ item.status.unavailableReplicas }}</span>
+
+      </td>
+      
+    </tr>
+  </tbody>
+</table>
+
 </template>
 
 <script setup lang='ts'>
