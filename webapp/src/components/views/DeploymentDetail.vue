640ebf4d73feca1f4f7a8de386c369bb587961df
tangx <uyinn@live.com>
2021-10-01 23:01:43 +0800

    feat: 自动获取 pod 详情

--- webapp/src/components/views/DeploymentDetail.vue
+++ webapp/src/components/views/DeploymentDetail.vue
@@ -3,46 +3,48 @@
   <div class="deployment-container">
     namespace: {{ data.Item.namespace }} &nbsp;
     deployment: {{ data.Item.name }} &nbsp;
-    <br>
-    replicas: <input type="text" v-model="data.Item.replicas">
-    <button @click="setDeploymentReplicas(data.Item)">设置</button>
+    <br />replicas:
+    <input type="text" v-model="data.Item.replicas" @keyup.enter="setDeploymentReplicas(data.Item)" />
+    <button @keyup.enter="setDeploymentReplicas(data.Item)">设置</button>
   </div>
 
   <div class="pod-container">
     <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Pod Name</th>
-      <th scope="col">Status</th>
-      <th scope="col">Node</th>
-      <th scope="col">Ipaddr</th>
-      <th scope="col">CreateTime</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="(pod,idx) of data.Pods" :key="pod.name">
-      <td>{{ pod.name }}</td>
-      <td>{{ pod.status.phase }}</td>
-      <td>{{ pod.nodeName }}</td>
-      <td>{{ pod.podIp }}</td>
-      <td>{{ pod.createTime }}</td>
-    </tr>
-  </tbody>
-</table>
+      <thead>
+        <tr>
+          <th scope="col">Pod Name</th>
+          <th scope="col">Status</th>
+          <th scope="col">Node</th>
+          <th scope="col">Ipaddr</th>
+          <th scope="col">CreateTime</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(pod,idx) of data.Pods" :key="pod.name">
+          <td>{{ pod.name }}</td>
+          <td>{{ pod.status.phase }}</td>
+          <td>{{ pod.nodeName }}</td>
+          <td>{{ pod.podIp }}</td>
+          <td>{{ pod.createTime }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script setup lang='ts'>import { reactive } from "@vue/reactivity";
-import { onMounted } from "@vue/runtime-core";
+import { onMounted, onUnmounted } from "@vue/runtime-core";
 import { useRouter } from "vue-router";
 import client, { Deployment } from "../../apis/deployment";
 
+// data 页面展示参数
 let data = reactive({
   Item: {} as Deployment,
   Error: "",
   Pods: [] as Pod[],
 })
 
+// req 路径参数与请求参数
 let req = reactive({
   Params: {
     name: "",
@@ -50,13 +52,27 @@ let req = reactive({
   }
 })
 
+// Swither 开关
+const Swither = reactive({
+  LoopData: false
+})
+
 const router = useRouter()
+
+const fetchDataLoop = async function () {
+  while (Swither.LoopData) {
+    getDeploymentPods()
+
+    await new Promise(f => setTimeout(f, 2000))
+  }
+  console.log("guodegang");
+
+}
 
 const fetchData = function () {
   getDeployment()
   getDeploymentPods()
 }
-
 
 const getDeployment = async function () {
   const p = req.Params
@@ -65,19 +81,23 @@ const getDeployment = async function () {
   data.Error = resp.error
 }
 
-const getDeploymentPods=async function(){
+const getDeploymentPods = async function () {
   const p = req.Params
 
-  let resp=await client.getDeploymentPodsByName(p.namespace,p.name)
-  data.Pods=resp.data
+  let resp = await client.getDeploymentPodsByName(p.namespace, p.name)
+  data.Pods = resp.data.sort((p1, p2): number => {
+    if (p1.name >= p2.name) {
+      return 1
+    }
+    return -1
+  })
 }
 
-const setDeploymentReplicas=function(item:Deployment){
+const setDeploymentReplicas = function (item: Deployment) {
   // console.log("item:::",item);
-  client.setDeploymentReplicas(item.namespace,item.name,item.replicas)
-
-  fetchData()
+  client.setDeploymentReplicas(item.namespace, item.name, item.replicas)
 }
+
 
 // 获取 url 中的变量信息
 const fetchUrlParams = function () {
@@ -97,17 +117,24 @@ const fetchUrlParams = function () {
 
 
 onMounted(() => {
+  Swither.LoopData = true
   fetchUrlParams()
 
   fetchData()
+  fetchDataLoop()
+})
+
+onUnmounted(() => {
+
+  Swither.LoopData = false
 })
 </script>
 
 <style lang='less' scoped>
-.deployment-container{
+.deployment-container {
   font-size: 20px;
   font-weight: 5000;
   // font: bold;
-  color: #0F65FD;
+  color: #0f65fd;
 }
 </style>
