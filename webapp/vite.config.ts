de410177b8112789bac9b45827fcdb0b0163db28
tangx <uyinn@live.com>
2021-11-03 16:13:16 +0800

    chore: update vite.config.ts

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -8,12 +8,13 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // resolv 对象中配置
-  resolve: {
-    // 别名配置
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    }
-  },
-  plugins: [vue()]
+    base: '/k8sailor/webapp/dist/',
+    // resolv 对象中配置
+    resolve: {
+        // 别名配置
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        }
+    },
+    plugins: [vue()]
 })
