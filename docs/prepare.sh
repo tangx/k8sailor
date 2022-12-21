4b498e1bd72ad8ed194b3c6937979fb793835202
Frank (Xin) Tang <f.tang@aligntech.com>
2022-12-21 17:40:59 +0800

    fix prepare.sh

--- docs/prepare.sh
+++ docs/prepare.sh
@@ -5,5 +5,5 @@ set -eux
 
 cd $(dirname $0)
 
-find . -type f -name "*.md" |xargs sed -i 's@(./assets@(../assets@g'
-# find . -type f -name "*.md" |xargs sed -i 's@(../assets@(../../assets@g'
+# find . -type f -name "*.md" |xargs sed -i 's@(./assets@(../assets@g'
+find . -type f -name "*.md" |xargs sed -i 's@(../assets@(../../assets@g'
